ad57e7f6d40a1dd9d86ece417160b66edfe0f30a
CarlosFranTorresMartinez <carlos.torres@vallegrande.edu.pe>
2022-04-25 03:49:13 -0500

    Rifas

--- src/reto/Rifas.java
+++ src/reto/Rifas.java
@@ -1,7 +1,13 @@
 package reto;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.DefaultListModel;
+import javax.swing.JOptionPane;
 
 public class Rifas extends javax.swing.JFrame {
 
@@ -32,11 +38,10 @@ public class Rifas extends javax.swing.JFrame {
         txtCantidadRifas = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
         btnSelectGanadores = new javax.swing.JButton();
-        jLabel2 = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        lsWinner = new javax.swing.JList<>();
+        jLabel7 = new javax.swing.JLabel();
+        jButton2 = new javax.swing.JButton();
         pnlRifas = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
         listRifas = new javax.swing.JList<>();
@@ -53,36 +58,49 @@ public class Rifas extends javax.swing.JFrame {
         jLabel1.setText("Cantidad de rifas a generar");
 
         btnSelectGanadores.setText("Seleccionar Ganadores");
+        btnSelectGanadores.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSelectGanadoresActionPerformed(evt);
+            }
+        });
 
-        jLabel2.setText("Premio 1");
+        jScrollPane2.setViewportView(lsWinner);
 
-        jLabel3.setText("Premio 3");
+        jLabel7.setText("GANADORES:");
 
-        jLabel4.setText("Premio 2");
-
-        jLabel5.setText("Premio 5");
-
-        jLabel6.setText("Premio 4");
+        jButton2.setText("Limpiar ");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout pnlMenuRifasLayout = new javax.swing.GroupLayout(pnlMenuRifas);
         pnlMenuRifas.setLayout(pnlMenuRifasLayout);
         pnlMenuRifasLayout.setHorizontalGroup(
             pnlMenuRifasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(pnlMenuRifasLayout.createSequentialGroup()
-                .addGap(22, 22, 22)
                 .addGroup(pnlMenuRifasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel5, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(pnlMenuRifasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addComponent(jLabel1)
-                        .addGroup(pnlMenuRifasLayout.createSequentialGroup()
-                            .addComponent(txtCantidadRifas, javax.swing.GroupLayout.PREFERRED_SIZE, 176, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addComponent(btnSelectGanadores, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(pnlMenuRifasLayout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addGroup(pnlMenuRifasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(pnlMenuRifasLayout.createSequentialGroup()
+                                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 316, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(0, 0, Short.MAX_VALUE))
+                            .addGroup(pnlMenuRifasLayout.createSequentialGroup()
+                                .addGroup(pnlMenuRifasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addComponent(jLabel1)
+                                    .addGroup(pnlMenuRifasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(btnSelectGanadores, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addComponent(txtCantidadRifas)))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                .addGroup(pnlMenuRifasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))))
+                    .addGroup(pnlMenuRifasLayout.createSequentialGroup()
+                        .addGap(127, 127, 127)
+                        .addComponent(jLabel7)
+                        .addGap(107, 107, 107)))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         pnlMenuRifasLayout.setVerticalGroup(
@@ -95,18 +113,13 @@ public class Rifas extends javax.swing.JFrame {
                     .addComponent(txtCantidadRifas, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(btnSelectGanadores)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel2)
+                .addGroup(pnlMenuRifasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSelectGanadores)
+                    .addComponent(jButton2))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel6)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel5)
-                .addContainerGap(14, Short.MAX_VALUE))
+                .addComponent(jLabel7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.DEFAULT_SIZE, 134, Short.MAX_VALUE))
         );
 
         jScrollPane1.setViewportView(listRifas);
@@ -143,16 +156,57 @@ public class Rifas extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        final int tomo = Integer.parseInt(txtCantidadRifas.getText());
+        List<String> list = new ArrayList<>();
 
-        for (int i = 0; i < tomo; i++) {
-            String generatorRifas = String.valueOf(generateNumberRandom());
-            listModel.addElement(addZeroLeft(generatorRifas));
+        if (txtCantidadRifas.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "No has ingresado la cantidad de los participantes");
+        } else if (Integer.valueOf(txtCantidadRifas.getText()) < 5) {
+            JOptionPane.showMessageDialog(null, "La cantidad de participantes no es suficiente");
+        } else {
+            final int tomo = Integer.parseInt(txtCantidadRifas.getText());
+            for (int i = 0; i < tomo; i++) {
+                String generatorRifas = String.valueOf(generateNumberRandom());
+                listModel.addElement(addZeroLeft(generatorRifas));
+            }
+
+            for (int i = 0; i < listModel.getSize(); i++) {
+                list.add(listModel.get(i));
+            }
+
+            Collections.sort(list);
+
+            listModel.removeAllElements();
+
+            for (String s : list) {
+                listModel.addElement(s);
+            }
+
+            listRifas.setModel(listModel);
+
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void btnSelectGanadoresActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSelectGanadoresActionPerformed
+        final HashSet<Integer> hashSet = new HashSet<>();
+        final DefaultListModel<String> listWinner = new DefaultListModel<>();
+
+        for (int i = 0; i < 5; i++) {
+            hashSet.add(random.nextInt(listModel.getSize()));
         }
 
-        listRifas.setModel(listModel);
+        while (hashSet.size() >= 5) {
+            hashSet.forEach(value -> {
+                listWinner.addElement(listModel.get(value));
+            });
+            lsWinner.setModel(listWinner);
+            break;
+        }
 
-    }//GEN-LAST:event_jButton1ActionPerformed
+    }//GEN-LAST:event_btnSelectGanadoresActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     public static void main(String args[]) {
         try {
@@ -176,14 +230,13 @@ public class Rifas extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnSelectGanadores;
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JList<String> listRifas;
+    private javax.swing.JList<String> lsWinner;
     private javax.swing.JPanel pnlMenuRifas;
     private javax.swing.JPanel pnlRifas;
     private javax.swing.JTextField txtCantidadRifas;
